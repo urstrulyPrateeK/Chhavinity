681632f111fc98a4cb4aa44da3fb71faf04b000c
Prateek Singh <emperorprateek74@gmail.com>
2025-07-21 19:44:52 +0530

    Changes : Setting up Steam and MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
+import { connect } from "mongoose";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001; 
@@ -14,4 +16,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB();
 });

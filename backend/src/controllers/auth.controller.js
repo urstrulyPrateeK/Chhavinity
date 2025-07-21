ac06e825444997fdbf5d26b0d1b8767dfcb7acd2
Prateek Singh <emperorprateek74@gmail.com>
2025-07-21 22:02:39 +0530

    Changes : Signin route completed in backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,55 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
 export async function signup(req, res) {
-    console.log("GET /api/auth/signup route hit");
-    res.send("Signup Route");
+  const { email, password, fullName } = req.body;
+
+  try {
+    if (!email || !password || !fullName) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 7) {
+      return res.status(400).json({ message: "Password must be at least 7 characters" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists, please use a diffrent one" });
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1; // generate a num between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    const newUser = await User.create({
+      email,
+      fullName,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function login(req, res) {
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
-import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001; 
 
 // Root route
+
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Welcome to Chhavinity Chat App Backend! 🚀");
 });

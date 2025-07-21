18bb5fcc314822dde9c1d9ac8a3bba3b3ecbb74a
Prateek Singh <emperorprateek74@gmail.com>
2025-07-21 23:10:52 +0530

    Implement onboarding functionality and protectRoute middleware for user authentication

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,6 @@
 import express from 'express';
-import { signup } from "../controllers/auth.controller.js";
-import { login } from "../controllers/auth.controller.js";
-import { logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, onboard } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -9,5 +8,11 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
 
+router.post("/onboarding", protectRoute, onboard);
+
+// check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;

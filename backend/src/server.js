8e83d7d83cf0e05fda99bc73ef090c111aeeabbb
Prateek Singh <emperorprateek74@gmail.com>
2025-07-21 23:29:27 +0530

    Changes : backend completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5001; 
@@ -9,11 +13,22 @@ const PORT = process.env.PORT || 5001;
 // Root route
 
 app.use(express.json());
+app.use(cookieParser());
 app.get("/", (req, res) => {
     res.send("Welcome to Chhavinity Chat App Backend! 🚀");
 });
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
